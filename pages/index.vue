6a81764589915fb123ec787c0a2adba628b54cf7
Eze323 <a.ezequielirace@gmail.com>
2024-08-10 22:36:53 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLayout>
+        <MainLayout>
+            <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+                IndexPage
+            </div>
+        </MainLayout>
+    </NuxtLayout>
+</template>
+
+
+<script setup>
+import MainLayout from '../layouts/default.vue'
+
+ </script>
